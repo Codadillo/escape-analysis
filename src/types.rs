31af5c08f3834a15ce5676dd9243c0c37e0b35d6
Leo Conrad-Shah <lconrads@ucsc.edu>
2023-12-05 22:16:14 -0800

    add poor type system

---
+++ src/types.rs
@@ -0,0 +1,64 @@
+use std::fmt;
+
+#[derive(Clone, PartialEq)]
+pub enum Type {
+    Tuple(Tuple),
+    Enum(Enum),
+}
+
+#[derive(Clone, PartialEq)]
+pub struct Tuple {
+    pub elems: Vec<Type>,
+}
+
+#[derive(Clone, PartialEq)]
+pub struct Enum {
+    pub variants: Vec<Type>,
+}
+
+impl Type {
+    pub fn unit() -> Self {
+        Self::Tuple(Tuple { elems: vec![] })
+    }
+}
+
+impl fmt::Debug for Type {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Tuple(t) => write!(f, "{t:?}"),
+            Self::Enum(e) => write!(f, "{e:?}"),
+        }
+    }
+}
+
+impl fmt::Debug for Enum {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[")?;
+        for (i, variant) in self.variants.iter().enumerate() {
+            write!(f, "{variant:?}")?;
+
+            if i + 1 != self.variants.len() {
+                write!(f, " | ")?;
+            }
+        }
+        write!(f, "]")?;
+
+        Ok(())
+    }
+}
+
+impl fmt::Debug for Tuple {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(")?;
+        for (i, elem) in self.elems.iter().enumerate() {
+            write!(f, "{elem:?}")?;
+
+            if i + 1 != self.elems.len() {
+                write!(f, ", ")?;
+            }
+        }
+        write!(f, ")")?;
+
+        Ok(())
+    }
+}
